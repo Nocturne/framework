e4769d2272ecc405a30a3bb458791189cf502c5b
metry <metry1@protonmail.com>
2024-09-04 18:56:36 -0700

    Add ordered and lazy loading
    fixed circular dependency

--- ControllerServiceLoader.lua
+++ ControllerServiceLoader.lua
@@ -1,38 +1,71 @@
 local ControllerServiceLoader = {}
 ControllerServiceLoader.__index = ControllerServiceLoader
 
---- Creates a new instance of ControllerServiceLoader
+--- Creates a new ControllerServiceLoader instance.
+-- @return ControllerServiceLoader A new instance of the loader.
 function ControllerServiceLoader.new()
     local self = setmetatable({}, ControllerServiceLoader)
-    self.controllers = {}
-    self.services = {}
+    self.controllers = setmetatable({}, { __index = self._loadModule })
+    self.services = setmetatable({}, { __index = self._loadModule })
+    self.loadingModules = {} -- Track modules being loaded to prevent circular dependencies.
     return self
 end
 
---- Loads a controller
----@param controllerName string The name of the controller to load
-function ControllerServiceLoader:loadController(controllerName)
-    -- Implement the logic to load a controller
-    print("Loading controller: " .. controllerName)
-    self.controllers[controllerName] = true
+--- Loads a list of modules in the specified order.
+-- Modules already loaded are skipped.
+-- @param moduleNames table An array of module names to load in order.
+function ControllerServiceLoader:loadInOrder(moduleNames)
+    for _, moduleName in ipairs(moduleNames) do
+        if not (self.controllers[moduleName] or self.services[moduleName]) then
+            self:_loadModule(nil, moduleName) 
+        end
+    end
 end
 
---- Loads a service
----@param serviceName string The name of the service to load
-function ControllerServiceLoader:loadService(serviceName)
-    -- Implement the logic to load a service
-    print("Loading service: " .. serviceName)
-    self.services[serviceName] = true
+--- Internal function to load a module on demand.
+-- Handles lazy loading and circular dependency checks.
+-- @param table (optional) The table to cache the loaded module in.
+-- @param moduleName string The name of the module to load.
+-- @return table The loaded module.
+-- @throws error If a circular dependency is detected.
+function ControllerServiceLoader:_loadModule(table, moduleName)
+    if self.loadingModules[moduleName] then
+        error("Circular dependency detected for module: " .. moduleName)
+    end
+
+    self.loadingModules[moduleName] = true 
+
+    -- Load the module and remove non-documentation comments
+    print("Loading module: " .. moduleName)
+    local moduleCode = require(moduleName)
+    moduleCode = moduleCode:gsub("%-%-[^%[].*", "") 
+    moduleCode = moduleCode:gsub("%-%-%[%[.-%]%]", "") 
+
+    local func, err = load(moduleCode, moduleName)
+    if not func then
+        error("Error loading module: " .. err)
+    end
+    local module = func()
+
+    self.loadingModules[moduleName] = nil 
+
+    if table then
+        table[moduleName] = module
+    end
+
+    return module
 end
 
---- Returns the list of loaded controllers
----@return table The list of loaded controllers
+--- Returns the table of loaded controllers.
+-- Controllers are lazily loaded when first accessed.
+-- @return table The table of loaded controllers.
 function ControllerServiceLoader:getLoadedControllers()
     return self.controllers
 end
 
---- Returns the list of loaded services
----@return table The list of loaded services
+--- Returns the table of loaded services.
+-- Services are lazily loaded when first accessed.
+-- @return table The table of loaded services.
 function ControllerServiceLoader:getLoadedServices()
     return self.services
 end
