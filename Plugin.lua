55fd46553b34556d87d24b1376dbf0195bccff23
metry <metry1@protonmail.com>
2024-09-05 11:35:17 -0700

    Plugins

---
+++ Plugin.lua
@@ -0,0 +1,45 @@
+
+local PluginManager = {}
+PluginManager.__index = PluginManager
+
+function PluginManager.new()
+    local self = setmetatable({}, PluginManager)
+    self.plugins = {}
+    return self
+end
+
+function PluginManager:loadPlugin(pluginPath)
+    local plugin, err = loadfile(pluginPath)
+    if not plugin then
+        print("Error loading plugin: " .. err)
+        return false
+    end
+
+    -- Disable harmful functionalities
+    local env = {
+        print = print,
+        pairs = pairs,
+        ipairs = ipairs,
+        -- TODO: Add more functions 
+    }
+    setfenv(plugin, env)
+
+    local success, result = pcall(plugin)
+    if not success then
+        print("Error executing plugin: " .. result)
+        return false
+    end
+
+    table.insert(self.plugins, result)
+    return true
+end
+
+function PluginManager:executePlugins()
+    for _, plugin in ipairs(self.plugins) do
+        if type(plugin.run) == "function" then
+            plugin.run()
+        end
+    end
+end
+
+return PluginManager
